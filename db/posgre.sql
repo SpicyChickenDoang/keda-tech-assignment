e3bdda0dc6b185fbe1d0ad2f315d7e38300bbce4
aidil rifqi <aidil.arm@gmail.com>
2022-11-26 13:32:04 +0700

    new db configs and queries

---
+++ db/posgre.sql
@@ -0,0 +1,11 @@
+-- DROP TABLE IF EXISTS public.parkingrecord;
+
+CREATE TABLE IF NOT EXISTS public.parkingrecord
+(
+    row_id smallint NOT NULL DEFAULT nextval('parkingrecord_row_id_seq'::regclass),
+    vehicle_type text COLLATE pg_catalog."default" NOT NULL,
+    login_time timestamp without time zone NOT NULL,
+    logout_time timestamp without time zone NOT NULL,
+    parking_cost bigint NOT NULL,
+    CONSTRAINT parkingrecord_pkey PRIMARY KEY (row_id)
+)
